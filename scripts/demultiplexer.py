5cc2f31d618d79cb4a106d9f12c868b9fcd9a276
Philip Hartout <philip.hartout@protonmail.com>
2020-12-16 16:43:50 +0100

    :sparkles: added demultiplexing script

---
+++ scripts/demultiplexer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""demultiplexer.py
+
+This script demultiplex the data, i.e. it creates a separate file for each
+cell containing the contigs.
+"""
+import os
+
+list_of_cells = list()
+
+
+def main():
+    for folder in os.listdir("../data/"):
+            if "preprocessed" not in folder:
+                with open(f"../data/{folder}/outs/filtered_contig.fasta", "r") as input:
+                    for line in input:
+                        if line.startswith(">"):
+                            cell = line.split("-1_contig")[0][1:]
+                            list_of_cells.append(cell)
+
+                        if not os.path.exists(f"../data/{folder}-preprocessed/"):
+                            os.mkdir(f"../data/{folder}-preprocessed/")
+
+                        with open(f"../data/{folder}-preprocessed/{cell}.fasta", "a") as output:
+                            output.write(line)
+
+                with open(f"../data/{folder}-preprocessed/list_of_cells.txt", "w") as file:
+                    file.write(str(list_of_cells))
+
+
+if __name__ == "__main__":
+    main()

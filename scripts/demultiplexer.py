55f4b43fc390976e24877f97c49956e7a8660421
Philip Hartout <philip.hartout@protonmail.com>
2020-12-17 18:31:52 +0100

    :open_file_folder: add adapted paths

--- scripts/demultiplexer.py
+++ scripts/demultiplexer.py
@@ -9,35 +9,42 @@ cell containing the contigs.
 From: https://github.com/Teichlab/bracer/issues/21
 
 """
+
+
 import os
+
 
 list_of_cells = list()
 
 
 def main():
-    for folder in os.listdir("../data/"):
+    for folder in os.listdir("../data/demultiplexed"):
         if "preprocessed" in folder:
             os.removedirs(folder)
 
-    for folder in os.listdir("../data/"):
+    for folder in os.listdir("../data/raw"):
         with open(
-            f"../data/{folder}/outs/filtered_contig.fasta", "r"
+            f"../data/raw/{folder}/outs/filtered_contig.fasta", "r"
         ) as input:
             for line in input:
                 if line.startswith(">"):
                     cell = line.split("-1_contig")[0][1:]
                     list_of_cells.append(cell)
 
-                if not os.path.exists(f"../data/{folder}-preprocessed/"):
-                    os.mkdir(f"../data/{folder}-preprocessed/")
+                if not os.path.exists(
+                    f"../data/demultiplexed/{folder}-preprocessed/"
+                ):
+                    os.mkdir(f"../data/demultiplexed/{folder}-preprocessed/")
 
                 with open(
-                    f"../data/{folder}-preprocessed/{cell}.fasta", "a"
+                    f"../data/demultiplexed/{folder}-preprocessed/{cell}.fasta",
+                    "a",
                 ) as output:
                     output.write(line)
 
         with open(
-            f"../data/{folder}-preprocessed/list_of_cells.txt", "w"
+            f"../data/demultiplexed/{folder}-preprocessed/list_of_cells.txt",
+            "w",
         ) as file:
             file.write(str(list_of_cells))
 

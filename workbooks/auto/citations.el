a902e6bfbd5710ee190dff2ebee666c0b8f46062
Philip Hartout <philip.hartout@protonmail.com>
2021-01-10 20:32:56 +0100

    :pencil: edits

---
+++ workbooks/auto/citations.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "citations"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "ramesh"
+    "jelcic"
+    "janeway"
+    "lindeman"
+    "stubbington"
+    "dendrou2015immunopathology"
+    "hawkins2007measuring"))
+ :bibtex)
+
